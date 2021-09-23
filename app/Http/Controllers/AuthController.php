db5810d3b5daccddd7bc5e01894a914ab75e04ec
Orirefa <refanurcahyo@gmail.com>
2021-09-23 15:09:56 +0700

    endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function index()
+    {
+        return [
+            'siswa' => [
+                'nis' => '3103119157',
+                'name' => 'Refa Dimas Nurcahyo',
+                'phone' => '082329697348',
+                'class' => 'XII RPL 5'
+            ]
+        ];
+    }
 }
